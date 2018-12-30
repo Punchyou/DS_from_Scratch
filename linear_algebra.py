315d0563f9c5e19286cafc1d8a4dcb4a6bb7e109
Maria Pantsiou <maria.th.pantsiou@gmal.com>
2018-12-30 19:05:11 +0000

    The dot product

---
+++ linear_algebra.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from functools import partial, reduce
+
+vectors = [[1, 3, 5], [3,5,9], [2,4,8]]
+v = [3,4,7]
+w = [2, 7, 9]
+''' Adding vectors. '''
+def vector_add(v, w):
+	return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+def vector_sum(vectors):
+    #sum all cooresponding elements
+	result = vectors[0]
+	for vector in vectors[1:]:
+		result = vector_add(result, vector)
+	return result
+
+#we prctically reduce the list of vectors, so
+def vector_sum(vectors):
+	return partial(reduce, vector_add)
+
+print(vector_sum(vectors))
+
+
+
+'''Multiplying vectors.'''
+
+#multiply   vector by a scalar
+def scalar_mulltiply(c, v):
+    """c is  number, v is a vector"""
+    return [c*v_i for v_i in v]
+    
+def vector_mean(v, w):
+    """compute the vector whose ith element
+    is the men of the ith elements of the input vectors."""
+    n = len(vectors)
+    return scalar_mulltiply(1/n, vector_sum(vectors))
+
+# the sum of vectors' coorespondingwise products - The dot product
+''' The dot product measure the length of the vector you'd get
+    if you projected v onto w.'''
+def dot(v, w):
+    """v_1*w_1 + v_2*w_2 + ... + v_n*w_n"""
+    return sum(v_i*w_i for v_i, w_i in zip(v,w))
+
+print(dot(v,w))
+
+# sum of squeares
+def sum_of_squares(v):
+    return dot(v, v)
+print(sum_of_squares(v))
+
