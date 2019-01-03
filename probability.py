e4d7de02c8ab7d5a800d4e3e7ab73f97424187da
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2019-01-03 00:27:54 +0000

    Various Normal cdfs

--- probability.py
+++ probability.py
@@ -8,7 +8,7 @@ Created on Wed Jan  2 20:47:42 2019
 
 """A family with two unknown children."""
 from random import choice, seed
-from math import sqrt, exp, pi
+from math import sqrt, exp, pi, erf
 from matplotlib import pyplot as plt
 
 def random_kid():
@@ -47,16 +47,28 @@ def uniform_cfd(x):
     else:
         return 1
 
-"""The normal Distribution"""
+"""The normal pdfs"""
 def normal_pdf(x, mu = 0, sigma = 1):
     sqrt_two_pi = sqrt(2 * pi)
     return (exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
 
 xs = [x / 10.0 for x in range(-50, 50)]
-plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label = 'mu=0, sigma=1')
+plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label = 'mu=0, sigma=1') #standart norma distribution
 plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '--', label = 'mu=0, sigma=2')
 plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ':', label = 'mu=0, sigma=0.5')
 plt.plot(xs, [normal_pdf(x, mu = -1) for x in xs], '-.', label = 'mu=-1, sigma=1')
 plt.legend()
 plt.title("Various Normal pdfs")
-plt.show()    
+plt.show()
+
+"""The norms cdfs"""
+def normal_cdf(x, mu=0, sigma=1):
+    return (1 + erf((x - mu) / sqrt(2) / sigma)) / 2 #using the erf, error function
+
+plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label = 'mu=0, sigma=1') #standart norma distribution
+plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label = 'mu=0, sigma=2')
+plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label = 'mu=0, sigma=0.5')
+plt.plot(xs, [normal_cdf(x, mu = -1) for x in xs], '-.', label = 'mu=-1, sigma=1')
+plt.legend(loc = 4) #bottom right
+plt.title("Various Normal cdfs")
+plt.show()
