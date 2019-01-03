5871fbfdfb55806a0ca40969377058a4798add34
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2019-01-03 18:20:52 +0000

    Using the Gradient

--- gradient_descent.py
+++ gradient_descent.py
@@ -6,7 +6,9 @@ Created on Thu Jan  3 17:23:00 2019
 """
 """Estimating the gradient"""
 from functools import partial
+from vectors import distance
 import matplotlib.pyplot as plt
+import random
 
 def difference_quotient(f, x, h):
     return (f(x + h) - f(x)) / h #as h approaches zero
@@ -36,4 +38,28 @@ def partial_derivative_quotent(f, v, i, h):
 
 def estimte_gradient(f, v, h=0.00001):
     return [partial_derivative_quotent(f, v, i, h)
-            for i, _ in enumerte(v)]
+            for i, _ in enumerate(v)]
+
+
+"""Using the gradient"""
+"""Pick a strting point nd thet tke tiny steps in the opposite direction
+untill the gradient is very small"""
+def step(v, direction, step_size):
+    """move step_size in the direction from v"""
+    return [v_i + step_size * direction_i
+            for v_i, direction_i in zip(v, direction)]
+
+def sum_of_squares_gradient(v):
+    return [2 * v_i for v_i in v]
+
+#pick a random starting point
+v = [random.randint(-10, 10) for i in range(3)]
+
+tolerance = 0.0000001
+
+while True:
+    gradient = sum_of_squares_gradient(v) #compute the gradient at v
+    next_v = step(v, gradient, -0.01) #take a negtive gradient step
+    if distance(next_v, v) < tolerance: #stop if you are converging
+        break
+    v = next_v #continue of you're not
