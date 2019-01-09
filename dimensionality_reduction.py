37f75595736676e81e3413f71cbe5ab1bb47be12
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2019-01-09 19:04:46 +0000

    Dimensionality reduction

--- dimensionality_reduction.py
+++ dimensionality_reduction.py
@@ -7,7 +7,9 @@ Created on Wed Jan  9 17:40:18 2019
 from matrices import shape
 from statistics2 import mean, standard_deviation
 from matrices import get_column
+from vectors import dot, vector_sum
 from working_wiith_data import make_matrix
+import numpy as np
 
 '''Principal component anaysis'''
 A = [[1, 2, 3], [2, 5, 8], [7, 9, 1]]
@@ -27,4 +29,24 @@ def de_mean_matrix(A):
     column_means, _ = scale(A)
     return make_matrix(nr, nc, lambda i, j: A[i][j] - column_means[j])
 
+def direction(w):
+    mag = np.linalg.norm(w)
+    return [w_i / mag for w_i in w]
 
+def directional_variance_i(x_i, w):
+    '''the variance of the data in the direction determined by w'''
+    return dot(x_i, direction(w)) ** 2
+
+def directional_variance(X, w):
+    '''the variamce of the data in the direction determined by w'''
+    return sum(directional_variance_i(x_i, w)
+               for x_i in X)
+
+'''maximize the variance'''
+def directional_variance_gradient_i(x_i, w):
+    '''the contribution of row x_i to the grdient of the direction-w variance'''
+    projection_length = dot(x_i, direction(w))
+    return [2 * projection_length * x_ij for x_ij in x_i]
+
+def directional_variance_gradient(X, w):
+    return vector_sum(directional_variance_gradient_i(x_i, w) for x_i in X)
